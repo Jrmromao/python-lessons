374d01eb79ee77919a9cd66fcf36109631fd0040
Joao Romao <Jrmromao@users.noreply.github.com>
2023-06-15 22:00:29 +0100

    Add files via upload

---
+++ iterations.py
@@ -0,0 +1,57 @@
+# Date: 15 Jun 2023
+# Author: Joao Romao & Carlos Romao
+
+# today we're are going to cover loops
+
+
+# We're going to practice english and programming at the same time
+
+
+# i for index
+
+
+# array []
+# len - Length //comprimento
+
+
+idades = [19, 1, 44, 78, 55]
+
+for idade in idades:
+    print(idade)
+
+if 6 == 9:
+
+    # for i in range(0, 13):
+    #     print(i)
+
+
+    age = 0
+
+
+# while age <= 10:
+#     age = int(input('Enter you age: '))
+#       print(age)
+
+# single line comment
+
+'''
+multi line comment
+'''
+
+
+#1  Imprimir uma sequência de números:
+
+#2 Calcular a soma de uma lista de valores:
+lista = [5, 2, 8, 3, 10]
+soma = 0
+#3 Verificar se um número é primo:
+numero = int(input("Digite um número: "))
+#4 Calcular o fatorial de um número: 8!
+# O fatorial de um número é o produto dele pelos seus antecessores maiores que 0
+
+numero = int(input("Digite um número: "))
+fatorial = 1
+
+#5 Imprimir os elementos de uma lista:
+frutas = ["maçã", "banana", "laranja", "uva"]
+#6 Imprimir os números pares de 1 a 10:
